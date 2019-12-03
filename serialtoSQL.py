01b9378c34b07acafdbba393be4c83de4b321e20
Ossi <ossi.miilukangas@hotmail.com>
2019-12-03 12:47:42 +0200

    toimii nyt valmiilla arvoilla

--- serialtoSQL.py
+++ serialtoSQL.py
@@ -16,40 +16,48 @@ serialPort = serial.Serial(port, baud, timeout = 1)
 
 #time.sleep(1)
 
-valueSum = 0.0
+tempSum = 0.0
+humiSum = 0.0
 count = 0
+temperature = ""
+humidity = ""
 
 while True:
     ser = str(serialPort.readline())
-    realValue = ser.replace("b", "").replace("'", "").replace(r"\r\n", "").replace("0.0.", "0.")
-    valueSum += float(realValue)
-    #time.sleep(.1)
-    count += 1
+    if(len(ser) == 21):
+        temperature = ser[14] + ser[15]
+        humidity = ser[8] + ser[9]
+        tempSum += float(temperature)
+        humiSum += float(humidity)
+        #time.sleep(.1)
+        count += 1
     
     if count == sample:
         cur.execute("SELECT MAX(idweather) FROM weather")
         currentID = str(cur.fetchall())
         currentID = int(currentID.replace("(", "").replace(",", "").replace(")", ""))
         
-        valueAvg = valueSum / sample
-        print("%.6f"%valueAvg)
+        tempAvg = tempSum / sample
+        humiAvg = humiSum / sample
+        print("%.6f "%tempAvg)
+        print("%.6f"%humiAvg)
         
         time = datetime.datetime.now().time()
         date = datetime.datetime.now().date()
         
-        if valueAvg < 1:
-            sql = ("INSERT INTO weather VALUES(%s, '%s', '%s', %.4f, %s)" % \
-                   (currentID + 1, date, time, valueAvg, 20))
-            try:
-                cur.execute(sql)
-                db.commit()
-                print("jes")
-            except:
-                db.rollback()
-                print("es")
+        sql = ("INSERT INTO weather VALUES(%s, '%s', '%s', %.4f, %s)" % \
+               (currentID + 1, date, time, tempAvg, humiAvg))
+        try:
+            cur.execute(sql)
+            db.commit()
+            print("jes")
+        except:
+            db.rollback()
+            print("es")
             
         count = 0
-        valueSum = 0.0
+        tempSum = 0.0
+        humiSum = 0.0
 
 serialPort.close()
 db.close()
