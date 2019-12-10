1f543adc338c9e8d13e190e2daaefd856f7aa49c
Ossi <ossi.miilukangas@hotmail.com>
2019-12-10 10:50:12 +0200

    serialtoSQl muokattu

--- serialtoSQL.py
+++ serialtoSQL.py
@@ -24,9 +24,14 @@ humidity = ""
 
 while True:
     ser = str(serialPort.readline())
-    if(len(ser) == 21):
-        temperature = ser[14] + ser[15]
-        humidity = ser[8] + ser[9]
+    if(len(ser) > 10 and len(ser) < 30): #15, 22 / 10, 30
+        try:
+            alku, humiTemp, humidity, temperature ,loppu = ser.split()
+        except:
+            humidity = 0;
+            temperature = 0;
+            count -= 1
+            
         tempSum += float(temperature)
         humiSum += float(humidity)
         #time.sleep(.1)
